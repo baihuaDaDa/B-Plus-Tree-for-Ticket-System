9f17c879bff36f2ca2dc6812868d5ec0c762ee25
baihua <zhangbohan_wx@sjtu.edu.cn>
2024-05-09 11:16:15 +0800

    Change the size of nodes and blocks.

--- src/BPT.hpp
+++ src/BPT.hpp
@@ -17,8 +17,8 @@ namespace baihua {
         using value_type = pair<Index, Value, CmpIndex, CmpValue>;
 
     private:
-        static constexpr int M = 200;
-        static constexpr int L = 1020;
+        static constexpr int M = 250;
+        static constexpr int L = 1270;
 
         struct Node {
             // one more for buffering
